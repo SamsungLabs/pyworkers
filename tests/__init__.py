c93ea92ea8e33fa67add7a44b65dde3009f0a36f
Łukasz Dudziak <l.dudziak@samsung.com>
2020-03-10 19:18:31 +0000

    [Misc] Remove commented code from __init__

--- tests/__init__.py
+++
@@ -1,8 +0,0 @@
-#import sys
-#from pathlib import Path
-#new_path = Path(__file__).parents[1].absolute()
-#sys.path = [new_path] + sys.path
-#try:
-#    import pyworkers
-#finally:
-#    sys.path = sys.path[1:]
