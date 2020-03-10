c688ba6697f059bb1b194decb01a2ab8e12cd7e2
Łukasz Dudziak <l.dudziak@samsung.com>
2020-03-10 19:45:53 +0000

    [Tests] Convert reduction test to unittest

--- tests/test_reduce.py
+++ tests/test_reduce.py
@@ -1,20 +1,17 @@
+import unittest
 import io
 import pickle
 import copyreg
 
-import sys
-from pathlib import Path
-sys.path.append(str(Path(__file__).parents[1]))
-
 import pickle
-import workers.remote_pickle as remote_pickle
+import pyworkers.remote_pickle as remote_pickle
 
 
-class Foo(remote_pickler.SupportRemoteGetState):
+class Foo(remote_pickle.SupportRemoteGetState):
     def __getstate__(self, remote=False):
         return 0 if not remote else 1
     def __setstate__(self, state):
-        print(type(self), state)
+        self.val = state
 
 class Bar(Foo):
     pass
@@ -23,30 +20,30 @@ class Dip():
     def __getstate__(self):
         return 2
     def __setstate__(self, state):
-        print(type(self), state)
+        self.val = state
 
 class Pap(Foo):
     def __getstate__(self, **kwargs):
         return super().__getstate__(**kwargs) * 10
 
-class Tup(remote_pickler.SupportRemoteGetState):
+class Tup(remote_pickle.SupportRemoteGetState):
     def __getstate__(self):
         return 4
     def __setstate__(self, state):
-        print(type(self), state)
+        self.val = state
 
 class Wep(Tup):
     def __getstate__(self, **kwargs):
         return super().__getstate__(**kwargs) * 100
 
-class WithPatchedState(remote_pickler.SupportRemoteGetState):
+class WithPatchedState(remote_pickle.SupportRemoteGetState):
     def __getstate__(self, remote=False):
         return { 'test': 1, 'test2': 10 } if remote else 0
 
     def __setstate__(self, state):
-        print(type(self), state)
+        self.val = state
 
-class NestedPatched1(remote_pickler.SupportRemoteGetState):
+class NestedPatched1(remote_pickle.SupportRemoteGetState):
     def __init__(self):
         self.special = 0
         self.normal = 1
@@ -64,12 +61,11 @@ class NestedPatched1(remote_pickler.SupportRemoteGetState):
             state['special'] = 2
 
         self.__dict__.update(state)
-        print(type(self), self.__dict__)
 
     def __repr__(self):
         return str(self.__dict__)
 
-class NestedPatched2(remote_pickler.SupportRemoteGetState):
+class NestedPatched2(remote_pickle.SupportRemoteGetState):
     def __init__(self):
         self.special = 0
         self.normal = 3
@@ -88,48 +84,96 @@ class NestedPatched2(remote_pickler.SupportRemoteGetState):
             state['special'] = 4
 
         self.__dict__.update(state)
-        print(type(self), self.__dict__)
 
 
-orig = (Bar(), Foo(), Dip(), Pap(), Tup(), Wep())
-print('Remote pickle')
-data = remote_pickler.dumps(orig)
-new = remote_pickler.loads(data)
+class RemotePickleTest(unittest.TestCase):
+    def test_simple_remote(self):
+        orig = (Foo(), Bar(), Dip(), Pap(), Tup(), Wep())
+        data = remote_pickle.dumps(orig)
+        new = remote_pickle.loads(data)
+        self.assertEqual((obj.val for obj in new)), (1, 1, 2, 10, 4, 400))
 
-print('Standard pickle')
-data2 = pickle.dumps(orig)
-new2 = pickle.loads(data2)
+    def test_simple_standard(self):
+        orig = (Foo(), Bar(), Dip(), Pap(), Tup(), Wep())
+        data = pickle.dumps(orig)
+        new = pickle.loads(data)
+        self.assertEqual((obj.val for obj in new), (0, 0, 2, 0, 4, 400))
 
-o = WithPatchedState()
-print('Patched remote')
-data3 = remote_pickler.dumps(o)
-new3 = remote_pickler.loads(data3, { 'test': 2 })
+    def test_patched_state_remote_not_patched(self):
+        orig = WithPatchedState()
+        data = remote_pickle.dumps(orig)
+        new = remote_pickle.loads(data)
+        self.assertIsInstance(new.val, dict)
+        self.assertEqual(new.val['test'], 1)
+        self.assertEqual(new.val['test2'], 10)
 
-print('Patched standard')
-data4 = pickle.dumps(o)
-new4 = pickle.loads(data4)
+    def test_patched_state_remote_patched(self):
+        orig = WithPatchedState()
+        data = remote_pickle.dumps(orig)
+        new = remote_pickle.loads(data, { 'test': 2 })
+        self.assertIsInstance(new.val, dict)
+        self.assertEqual(new.val['test'], 2)
+        self.assertEqual(new.val['test2'], 10)
 
-o2 = NestedPatched2()
-print('Nested remote')
-data5 = remote_pickler.dumps(o2)
-new5 = remote_pickler.loads(data5)
+    def test_patched_state_standard(self):
+        orig = WithPatchedState()
+        data = pickle.dumps(orig)
+        new = pickle.loads(data)
+        self.assertEqual(new.val, 0)
 
-print('Nested patched remote')
-data6 = remote_pickler.dumps(o2)
-new6 = remote_pickler.loads(data6, { 'special': 8, 'nested': { 'special': 16 } })
+    def test_nested_patched_remote_not_patched(self):
+        orig = NestedPatched2()
+        data = remote_pickle.dumps(orig)
+        new = remote_pickle.loads(data)
+        self.assertEqual(new.nested.normal, 1)
+        self.assertEqual(new.nested.special, 2)
+        self.assertEqual(new.normal, 3)
+        self.assertEqual(new.special, 4)
 
-print('Nested standard')
-data7 = pickle.dumps(o2)
-new7 = pickle.loads(data7)
+    def test_nested_patched_remote_patched_both(self):
+        orig = NestedPatched2()
+        data = remote_pickle.dumps(orig)
+        new = remote_pickle.loads(data, { 'special': 8, 'nested': { 'special': 16 } })
+        self.assertEqual(new.nested.normal, 1)
+        self.assertEqual(new.nested.special, 16)
+        self.assertEqual(new.normal, 3)
+        self.assertEqual(new.special, 8)
 
-print('Nested patched remote 2')
-data8 = remote_pickler.dumps(o2)
-new8 = remote_pickler.loads(data8, { 'nested': { 'special': 16 } })
+    def test_nested_patched_remote_patched_nested(self):
+        orig = NestedPatched2()
+        data = remote_pickle.dumps(orig)
+        new = remote_pickle.loads(data, { 'nested': { 'special': 16 } })
+        self.assertEqual(new.nested.normal, 1)
+        self.assertEqual(new.nested.special, 16)
+        self.assertEqual(new.normal, 3)
+        self.assertEqual(new.special, 4)
 
-print('Nested patched remote 3')
-data9 = remote_pickler.dumps(o2)
-new9 = remote_pickler.loads(data9, { 'nested': -1 })
+    def test_nested_patched_remote_patched_top(self):
+        orig = NestedPatched2()
+        data = remote_pickle.dumps(orig)
+        new = remote_pickle.loads(data, { 'special': 8 })
+        self.assertEqual(new.nested.normal, 1)
+        self.assertEqual(new.nested.special, 2)
+        self.assertEqual(new.normal, 3)
+        self.assertEqual(new.special, 8)
 
-print('Nested patched remote 4')
-data10 = remote_pickler.dumps(o2)
-new10 = remote_pickler.loads(data10, { 'special': 8 })
+    def test_nested_patched_remote_override_nested(self):
+        orig = NestedPatched2()
+        data = remote_pickle.dumps(orig)
+        new = remote_pickle.loads(data, { 'nested': -1 })
+        self.assertEqual(new.nested, -1)
+        self.assertEqual(new.normal, 3)
+        self.assertEqual(new.special, 4)
+
+    def test_nested_patched_standard(self):
+        orig = NestedPatched2()
+        data = pickle.dumps(orig)
+        new = pickle.loads(data)
+        self.assertEqual(new.nested.normal, 1)
+        self.assertEqual(new.nested.special, 0)
+        self.assertEqual(new.normal, 3)
+        self.assertEqual(new.special, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
