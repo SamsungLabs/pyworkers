3f0c46248662f6cf2fd748ff5739942a75e2b961
Lukasz Dudziak/Embedded AI /SRUK/Engineer/Samsung Electronics <l.dudziak@samsung.com>
2020-03-28 15:49:44 +0000

    [Misc] Make version queries lazy

--- pyworkers/__init__.py
+++ pyworkers/__init__.py
@@ -1,1 +1,27 @@
-from .version import *
+from .utils import add_module_properties, staticproperty
+
+
+def _get_version():
+    from . import version
+    return version.version
+
+def _get_has_repo():
+    from . import version
+    return version.has_repo
+
+def _get_repo():
+    from . import version
+    return version.repo
+
+def _get_commit():
+    from . import version
+    return version.commit
+
+
+add_module_properties(__name__, {
+    '__version__': staticproperty(_get_version),
+    '__has_repo__': staticproperty(_get_has_repo),
+    '__repo__': staticproperty(_get_repo),
+    '__commit__': staticproperty(_get_commit)
+})
+
