526922b49ee52238d4cbcf5c119de4dbd375f69b
Lukasz Dudziak/Embedded AI /SRUK/Engineer/Samsung Electronics <l.dudziak@samsung.com>
2020-03-31 20:10:04 +0100

    [Misc] Update setup script to match new version

--- setup.py
+++ setup.py
@@ -6,10 +6,12 @@ from setuptools.command.install import install
 import importlib.util
 from pathlib import Path
 
-version_file = Path(__file__).parent.joinpath('pyworkers', 'version.py')
-spec = importlib.util.spec_from_file_location('pyworkers_version', version_file)
-pyworkers_version = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(pyworkers_version)
+package_name = 'pyworkers'
+
+version_file = Path(__file__).parent.joinpath(package_name, 'version.py')
+spec = importlib.util.spec_from_file_location('{}.version'.format(package_name), version_file)
+package_version = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(package_version)
 
 class build_maybe_inplace(install):
     def run(self):
@@ -17,13 +19,13 @@ class build_maybe_inplace(install):
         assert not _dist_file.exists()
         #if not _dist_file.exists():
         with open(_dist_file, 'w') as df:
-            df.write('\n'.join(map(lambda attr_name: attr_name+' = '+repr(getattr(pyworkers_version, attr_name)), pyworkers_version.__all__)) + '\n')
+            df.write('\n'.join(map(lambda attr_name: attr_name+' = '+repr(getattr(package_version, attr_name)), package_version.__all__)) + '\n')
 
         return super().run()
 
 
 setup(name='PyWorkers',
-      version=pyworkers_version.__version__,
+      version=package_version.__version__,
       description='Library providing abstraction over different types of parallel jobs: threads, processes and remote processes.',
       author='Łukasz Dudziak (SAIC-Cambridge, On-Device Team)',
       author_email='l.dudziak@samsung.com',
